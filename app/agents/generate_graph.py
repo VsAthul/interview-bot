91ac8dd6444962678a7f4c60d341dc175aae3504
ATHUL V.S <athulvs513@gmail.com>
2026-05-22 13:24:16 +0530

    updates

---
+++ app/agents/generate_graph.py
@@ -0,0 +1,7 @@
+from app.agents.graph import build_graph
+
+compiled_graph = build_graph()
+graph_image = compiled_graph.get_graph().draw_mermaid_png()
+with open("graph_image.png","wb") as f:
+    f.write(graph_image)
+
